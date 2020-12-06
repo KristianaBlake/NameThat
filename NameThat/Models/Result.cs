2e2e8f64ef78eff6b8f10bd5f8e58d2f5e5fee28
Kristiana Blake <kristiana.blake@gmail.com>
2020-12-06 17:22:05 -0600

    created Result model

---
+++ NameThat/Models/Result.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace NameThat.Models
+{
+    public class Result
+    {
+        public string Type { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public string Title { get; set; }
+
+        public int YearOfRelease { get; set; }
+
+    }
+}
