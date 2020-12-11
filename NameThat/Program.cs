b9bb382f24c396c1c66321584bba6750738f13e7
Kristiana Blake <kristiana.blake@gmail.com>
2020-12-11 01:22:19 -0600

    updated Program.cs

--- NameThat/Program.cs
+++ NameThat/Program.cs
@@ -1,26 +1,57 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace NameThat
 {
-    public class Program
+    public class OmdbAPI
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public string Title { get; set; }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public string Type { get; set; }
+
+        public string Picture { get; set; }
+
+        public int YearOfRelease { get; set; }
     }
+
+    class Program 
+    {
+        public static void btnSearch_Click(object sender, EventArgs e)
+        {
+            string userInput = e.ToString();
+
+            if (userInput == null || userInput.Length == 0)
+            {
+                throw new ApplicationException("The title you have entered Does Not Exist");
+            }
+            else
+            {
+                string apiKey = "942af2e";
+                string searchUrl = $"http://www.omdbapi.com/?t=" + userInput + "&apikey=" + apiKey;
+
+                using (WebClient wc = new WebClient())
+                {
+                    string json = wc.DownloadString(searchUrl);
+                    JavaScriptSerializer oJS = new JavaScriptSerializer();
+                    Program obj - new Program();
+                    obj = oJS. Deserialize<OmdbAPI>(json);
+                    if (obj.Response == "True")
+                    {
+                        OmdbAPI.Title == obj.Title;
+                        OmdbAPI.Type == obj.Type;
+                        OmdbAPI.Picture == obj.Picture;
+                        OmdbAPI.YearOfRelease == obj.YearOfRelease
+                    }
+                    else
+                    {
+                        OmdbSearchAPI.Title == "";
+                        OmdbSearchAPI.Type == "";
+                        OmdbSearchAPI.Picture == "";
+                        OmdbSearchAPI.YearOfRelease == "";
+                    }
+                }
+                    }
+                }
+    }
+
 }
+
